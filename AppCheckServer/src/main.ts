377a7036daf29a4598f9768ee5f797374e72072c
Denver Coneybeare <dconeybe@google.com>
2024-06-05 15:45:37 +0000

    got connected

--- AppCheckServer/src/main.ts
+++ AppCheckServer/src/main.ts
@@ -1,1 +1,24 @@
-console.log('Hello World');
+import { initializeApp } from 'firebase-admin/app';
+import { Firestore } from 'firebase-admin/firestore';
+import * as signale from 'signale';
+
+const logger = new signale.Signale({
+  config: {
+    displayTimestamp: true
+  }
+});
+
+async function main() {
+  logger.info('Initializing firebase-admin sdk');
+  const app = initializeApp();
+
+  const firestore = new Firestore(app);
+  logger.info('Getting collection contents');
+  const snapshot = await firestore.collection('AndroidIssue5101').get();
+  logger.info(`Got ${snapshot.size} documents`);
+  for (const document of snapshot.docs) {
+    logger.note(document.ref.path);
+  }
+}
+
+main();
